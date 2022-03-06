4d97ad52111bac44f6e9303a21cf641b31835511
dhwaniPatel <dhwanipatel1911@gmail.com>
2022-03-06 16:14:02 -0500

    defing functions in apriori

---
+++ apriori.py
@@ -0,0 +1,34 @@
+import this
+
+class Apriori_algo:
+    def __init__(self, inputFile = None,dataChunk =100, support = 10 ):
+        self.inputSize = inputFile
+        self.dataChunk = dataChunk
+        self.support = support
+
+    def fileLength(self):
+        with open(self.inputFile) as file:
+            for i in enumerate(file):
+                pass
+        
+        return i + 1
+
+    #getter
+    def support(self):
+        return self.support
+    
+    #setter
+    def support(self, s):
+        this.support = int((s*self.dataChunk)//100)
+
+    
+    def dataChunk(self):
+        return self.dataChunk
+    
+    def dataChunk(self, data):
+        buckets = self.fileLength()
+        self.dataChunk = int((data*buckets)//100)
+
+    
+
+    
